19e1b6dc324846ea5d73ecc46f55a1f65c375c5c
ew0s <ewos365@mail.ru>
2021-09-25 15:00:06 +0300

    Add: 16-30\ tasks lab1

--- lab1/TaskScript.sh
+++ lab1/TaskScript.sh
@@ -6,24 +6,32 @@ linksPath="${HOME}/test/links"
 
 #1
 cd $HOME && mkdir test 2> /dev/null && cd test && touch list 2> /dev/null
+
 #2
 cd $HOME/Desktop/test && ls -aFG > $listPath
+
 #3
 ls -d /etc/* | wc -l >> $listPath
 sudo find /etc/ -name ".*" | wc -l >> $listPath
+
 #4
 mkdir $testPath/links 2> /dev/null
+
 #5
 cd $testPath/links
 ln $listPath list_hlink 2> /dev/null
+
 #6
 ln -s $listPath list_slink 2> /dev/null
+
 #7
 ls -li list_hlink
 ls -li $listPath
 ls -li list_slink
+
 #8
 wc -l < $listPath >> list_hlink
+
 #9
 difference=$(diff list_hlink list_slink 2> /dev/null)
 if [[ "$?" = "0" ]]; then
@@ -31,8 +39,10 @@ if [[ "$?" = "0" ]]; then
         echo "YES"
     fi
 fi
+
 #10
 mv $listPath $testPath/list1
+
 #11
 difference=$(diff list_hlink list_slink 2> /dev/null)
 if [[ "$?" = "0" ]]; then
@@ -40,13 +50,71 @@ if [[ "$?" = "0" ]]; then
         echo "YES"
     fi
 fi
+
 #12
+
 #13
 listConfPath=$HOME/list_conf
 sudo find /etc/ -iname "*.conf" > $listConfPath
+
 #14
 listdPath=$HOME/list_d
 sudo find /etc/ -d -iname "*.d" > $listdPath
+
 #15
 listConfDPath=$HOME/list_conf_d
 cat $listConfPath >> $listConfDPath && cat $listdPath >> $listConfDPath
+
+#16
+cd $testPath
+mkdir .sub
+
+#17
+cp ~/list_conf_d ~/test/.sub
+
+#18
+rsync ~/list_conf_d ~/test/.sub
+
+#19
+ls -l ~/test
+
+#20
+man man > ~/man.txt
+
+#21
+split -b 1k  ~/man.txt
+
+#22
+mkdir ~/test/man.dir
+
+#23
+mv ~/x* ~/test/man.dir
+
+#24
+cat ~/test/man.dir/x* > ~/test/man.dir/man.txt
+
+#25
+if diff ~/man.txt ~/test/man.dir/man.txt; then
+	echo "YES"
+fi
+
+#26
+sed -i -e '1 s/^/string1\n/;' ~/man.txt
+sed -i -e '1 s/^/string2\n/;' ~/man.txt
+echo "string-2" >> ~/man.txt
+echo "string-1" >> ~/man.txt
+
+#27
+diff -u ~/man.txt ~/test/man.dir/man.txt > ~/filesdiff
+
+#28
+mv ~/filesdiff ~/test/man.dir
+
+#29
+patch ~/test/man.dir/man.txt filesdiff
+
+#30
+if diff ~/man.txt ~/test/man.dir/man.txt; then
+	echo "YES"
+fi
+
