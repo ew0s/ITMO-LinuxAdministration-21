3c7024cc6d74b69e6c7219a380657dde49418e5d
ew0s <ewos365@mail.ru>
2021-09-25 12:17:09 +0300

    Add: 4-7 tasks lab1

--- lab1/TaskScript.sh
+++ lab1/TaskScript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+testPath="${HOME}/test"
 listPath="${HOME}/test/list"
 
 #1
@@ -9,4 +10,15 @@ cd $HOME/Desktop/test && ls -aFG > $listPath
 #3
 ls -d /etc/* | wc -l >> $listPath
 find /etc -name ".*" | wc -l >> $listPath
+#4
+mkdir $testPath/links
+#5
+cd $testPath/links
+ln $listPath list_hlink
+#6
+ln -s $listPath list_slink
+#7
+ls -li list_hlink
+ls -li $listPath
+ls -li list_slink
 
